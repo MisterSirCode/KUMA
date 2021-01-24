0c524dfa58ff3a686559d058b8dd874fae8c47e9
Sir Code <taylorschneider07@yahoo.com>
2021-01-24 15:07:16 -0500

    Update math.js

--- Commands/math.js
+++ Commands/math.js
@@ -1,17 +1,18 @@
 const Discord = require("discord.js");
-const { evaluate } = require('mathjs')''
+const { evaluate } = require('mathjs');
 
 module.exports = {
     name: "m",
     description: "",
     execute(msg, args, Bot, Color, Version, Prefix) {
-        try {
-            const mathEmbed = new Discord.MessageEmbed()
-                .setColor(Color)
-                .setTitle(`${evaluate(args)}`);
-            msg.channel.send(mathEmbed);
-        } catch(e) {
-            // overflow errors begone!
-        }
+        // try {
+        //     const mathEmbed = new Discord.MessageEmbed()
+        //         .setColor(Color)
+        //         .setTitle(`${evaluate(args)}`);
+        //     msg.channel.send(mathEmbed);
+        // } catch(e) {
+        //     // overflow errors begone!
+        // }
+        msg.channel.send("Due to Major Vulnerabilities discovered in MathJS, this command has been disabled for now");
     }
 };
