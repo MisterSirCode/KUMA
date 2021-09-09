f60c6c83aabd5c915a6c378f23bca0d59f3cf0d7
Sir Code <taylorschneider07@yahoo.com>
2021-09-08 20:28:46 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 const commandBuilder = new SlashCommandBuilder()
     .setName('ban')
     .setDescription('(Guild Moderator) Ban user from your guild')
-    .addMUserOption((option) => 
+    .addUserOption((option) => 
         option.setName('user')
         .setDescription('User to ban')
         .setRequired(true));
@@ -16,7 +16,7 @@ module.exports = {
             const failEmbed = new MessageEmbed()
                 .setTitle(`You cant ban a user outside a guild`)
                 .setColor(global.color);
-            await interaction.reply({ embeds: [failEmbed], ephemeral: true });
+            interaction.reply({ embeds: [failEmbed], ephemeral: true });
             return;
         }
         if (interaction.user.member.permissions.has('BAN_MEMBERS')) {
@@ -29,7 +29,7 @@ module.exports = {
                         .setAuthor(`${userOption.username}#${userOption.descriminator}`, `https://cdn.discordapp.com/avatars/${userOption.id}/${userOption.avatar}.png`)
                         .setTitle(`${userOption.username} was banned from the server`)
                         .setColor(global.color);
-                    await interaction.reply({ embeds: [banEmbed], ephemeral: true });
+                    interaction.reply({ embeds: [banEmbed], ephemeral: true });
                     return;
                 })
                 .catch((e) => interaction.reply({ content: `Ban failed with error, ${e}`, ephemeral: true}));
